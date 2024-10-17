db12e1d01487838c8b19deff35fd1f320522c6f0
gaura <gaurab.dhimal35@gmail.com>
2024-10-17 13:35:14 -0400

    4th Method done

--- src/DevTest.java
+++ src/DevTest.java
@@ -9,6 +9,7 @@ public class DevTest
         int age = 0;
         int favNum = 0;
         double salary = 0;
+        double constraint = 0; // must be between 100-10000
 
         firstName = getNonZeroLenString(in, "Enter your first name");
         System.out.println("Fname is " + firstName);
@@ -24,6 +25,10 @@ public class DevTest
 
         favNum = getRangedInt(in, "Enter your favorite number", 1, 10);
         System.out.println("Your favorite number is " + favNum);
+
+
+        constraint = getRangedDouble(in, "Enter the constraint double", 100, 10000);
+        System.out.println("Constraint is " + constraint);
     }
 
     public static String getNonZeroLenString(Scanner pipe, String prompt)
@@ -131,6 +136,48 @@ public class DevTest
             {
                 trash = pipe.nextLine();
                 System.out.println("You must enter a valid int, not " + trash);
+            }
+
+        }while(!done);
+
+        return retVal;
+    }
+
+
+    /**
+     * Get a double from the user via the console within a specified range
+     * @param pipe the scanner to use for input
+     * @param prompt the prompt to tell the user what is required
+     * @param low the inclusive low bound
+     * @param high the inclusive high bound
+     * @return a double within the specified bounds
+     */
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double retVal = 0;
+        boolean done = false;
+        String trash = "";
+
+        do
+        {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if(pipe.hasNextDouble()) {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+
+                if(retVal >= low && retVal <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("You must enter a value within the range [" + low + " - " + high + "]: ");
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a valid double, not " + trash);
             }
 
         }while(!done);
